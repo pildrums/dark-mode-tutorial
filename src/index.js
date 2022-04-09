7d0235618582578518b3d8162e890386f021d5dd
김필진 <pildrum@pildrum.local>
2022-04-09 18:35:51 +0900

    gh-pages add

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import 'index.css';
 import App from 'App';
 
+// yarn add gh-pages styled-components react-icons
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
